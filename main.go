d3fbaa09fe5b46f639b8c122351e70f4729fb727
CallumB04 <callumburgoyne04@gmail.com>
2025-09-04 12:16:47 +0300

    feat: create basic server with root path response

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("This works!..")
+	http.HandleFunc("/", handleRoot) // assign handler for root path ("/")
+
+	fmt.Println("Server is running at localhost:8080")
+	http.ListenAndServe(":8080", nil) // start server on port 8080
+}
+
+// define behaviour for requests to root path ("/")
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
