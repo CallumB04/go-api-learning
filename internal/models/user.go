30f27cffdee75f55829479924a92e25564c0b24a
CallumB04 <callumburgoyne04@gmail.com>
2025-09-05 03:15:59 +0300

    feat: update user data structure

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,32 +1,28 @@
 package models
 
+import "time"
+
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Picture  string `json:"picture"`
-	Bio      string `json:"bio"`
-	Online   bool   `json:"online"`
+	ID        string    `json:"id"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	Bio       string    `json:"bio"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 var TestUsers = []User{
 	{
-		ID:       "1",
-		Username: "callum",
-		Email:    "thisisanemail@gmail.com",
-		Password: "Password123",
-		Picture:  "",
-		Bio:      "i love playrates!!!!",
-		Online:   true,
+		ID:        "1",
+		Email:     "thisisanemail@gmail.com",
+		Username:  "callum",
+		Bio:       "This is a test bio",
+		CreatedAt: time.Now().UTC(),
 	},
 	{
-		ID:       "2",
-		Username: "testaccount",
-		Email:    "otheremail@gmail.com",
-		Password: "Password321",
-		Picture:  "",
-		Bio:      "",
-		Online:   false,
+		ID:        "2",
+		Email:     "otheremail@gmail.com",
+		Username:  "testperson",
+		Bio:       "",
+		CreatedAt: time.Now().UTC(),
 	},
 }
